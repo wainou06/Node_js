7684d7594da47c7af6076766a0b47d5147173399
wainou06 <wainou.6@gmail.com>
2025-07-09 15:52:45 +0900

    up

--- ch04/routes/comments.js
+++ ch04/routes/comments.js
@@ -1,4 +1,75 @@
 const express = require('express')
+const Comment = require('../models/comment')
 const router = express.Router()
+
+// 새로운 댓글 등록
+// localhost:8000/comments
+router.post('/', async (req, res, next) => {
+   try {
+      const comment = await Comment.create({
+         comment: req.body.comment, // 댓글 내용
+         commenter: req.body.id, // 댓글 작성자 id
+      })
+   } catch (error) {
+      console.error(error)
+      next(error)
+   }
+})
+
+// 댓글 수정
+// :id 댓글 아이디
+// localhost:8000/comments/:id
+router.patch('/:id', async (req, res, next) => {
+   try {
+      // update comments set comment = '수정할 내용' where id = :id
+      const result = await Comment.update(
+         {
+            comment: req.body.comment, // 수정할 댓글 내용
+         },
+         {
+            where: { id: req.params.id }, // 수정할 댓글 id
+         }
+      )
+      console.log(result)
+      // result[0]: 수정된 레코드의 갯수
+      if (result[0] === 0) {
+         {
+            // 수정이 안된 경우
+            return res.status(404).json({ massage: '댓글을 찾을 수 없습니다.' })
+         }
+      }
+
+      // 댓글이 정상적으로 수정된 경우
+      res.status(200).json({ message: '댓글이 수정되었습니다', result })
+   } catch (error) {
+      console.error(error)
+      next(error)
+   }
+})
+
+// 댓글 삭제
+// localhost:8000/comments/:id
+router.delete('/:id', async (req, res, next) => {
+   try {
+      // delete from comments where id = :id
+      const result = await Comment.destroy({
+         where: { id: req.params.id },
+      })
+
+      console.log(result)
+
+      // result: 삭제된 레코드의 갯수
+      if (result === 0) {
+         // 삭제된 데이터가 없는 경우
+         return res.status(404).json({ message: '댓글을 찾을 수 없습니다.' })
+      }
+
+      // 댓글이 정상적으로 삭제된 경우
+      res.status(200).json({ message: '댓글이 삭제되었습니다.', result })
+   } catch (error) {
+      console.error(error)
+      next(error)
+   }
+})
 
 module.exports = router
--- ch04/routes/users.js
+++ ch04/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const Comment = require('../models/comment')
 const router = express.Router()
 
 // 모든 사용자
@@ -28,6 +29,62 @@ router.get('/:id', async (req, res, next) => {
       })
       console.log(user)
       res.status(200).json(user)
+   } catch (error) {
+      console.error(error)
+      next(error)
+   }
+})
+
+// 특정 사용자가 작성한 모든 댓글 가져오기
+// localhost:8000/users/:id/comments
+router.get('/:id/comments', async (req, res, next) => {
+   /* 
+   select * from comments c , users u 
+   where c.commenter = u.id
+   and u.id = 3;
+   */
+   try {
+      // comments 테이블 데이터를 가져올때 특정 id를 가진 users 테이블 데이터를 포함해서 가져온다
+      // const comments = await Comment.findAll({
+      //    // 특정 컬럼만 조회
+      //    attributes: ['comment', 'create_at'],
+      //    // include 안에 있는건 User에 대한것만 작성
+      //    include: {
+      //       model: User,
+      //       where: { id: req.params.id },
+      //       attributes: ['name', 'age'],
+      //    },
+      // })
+
+      const comments = await User.findAll({
+         where: { id: req.params.id },
+         include: {
+            model: Comment,
+            attributes: ['comment', 'create_at'],
+         },
+      })
+      console.log(comments)
+      res.status(200).json(comments)
+   } catch (error) {
+      console.error(error)
+      next(error)
+   }
+})
+
+// 특정 사용자 등록
+router.post('/', async (req, res, next) => {
+   try {
+      console.log(req.body)
+
+      const user = await User.create({
+         name: req.body.name, // 클라이언트 받은 값들을 컬럼값으로 지정
+         age: req.body.age,
+         married: req.body.married,
+         comment: req.body.comment,
+      })
+
+      console.log(user)
+      res.status(201).json(user) // create 성공시에는 상태코드 201, 생성된 사용자 데이터 response
    } catch (error) {
       console.error(error)
       next(error)
